ff12fbaf0aeb47db8d5d41d810c2360189d13b05
Brian G <brian.gregorius@gmail.com>
2018-12-04 13:22:37 -0500

    first commit

---
+++ helper.js
@@ -0,0 +1,11 @@
+const areSameLength = (str1, str2) => {
+    if (typeof str1 != 'string' || typeof str2 != 'string' ){
+        return null}else {
+            return str1.length === str2.length;
+    }
+};
+
+
+module.exports = {
+    areSameLength,
+};
---
+++ helpers.spec.js
@@ -0,0 +1,44 @@
+const helpers = require('./helper.js');
+
+describe('helpers library', () => {
+    
+    describe('add function', () => {
+        it('can add two numbers', () => {
+
+        });
+        //low hanging fruit
+        //obvious happy path
+        //obvious sad path
+        //edge/corner cases
+    });
+    describe('areSameLength function', () => {
+        
+    it('should return true when called with strings of the same length', () =>{
+        expect(helpers.areSameLength('a', 'a')).toBe(true);
+    });
+
+    it('should return false when called with strings of differnt length', () =>{
+        expect(helpers.areSameLength('a', 'ab')).toBe(false);
+    });
+
+    it('should return null when any of the arugments are not strings', () =>{
+        expect(helpers.areSameLength(null, 'a')).toBe(null);
+        expect(helpers.areSameLength([], 'a')).toBe(null);
+        expect(helpers.areSameLength(1, 'a')).toBe(null);
+        expect(helpers.areSameLength('1', NaN)).toBe(null);
+        expect(helpers.areSameLength(1, {})).toBe(null);
+    });
+
+    // check for different length strings
+    // it's not a string
+    //passing null or underfined
+    //passing no arguments
+    //passing non strings
+        //passing numbers
+        //passing arrays
+        //passing objects
+    });
+
+
+});
+
